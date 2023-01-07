5a166fe2e80f0fb936efe940f435ca61ca709663
Scott Griv <scottgrivner@gmail.com>
2023-01-06 21:12:55 -0500

    Update palm_tree.sql

--- sql/palm_tree.sql
+++ sql/palm_tree.sql
@@ -41,7 +41,7 @@ CREATE TABLE `about` (
 
 LOCK TABLES `about` WRITE;
 /*!40000 ALTER TABLE `about` DISABLE KEYS */;
-INSERT INTO `about` VALUES (1,'Scott Grivner','scott.grivner@gmail.com','www.scottgrivner.com','1.0.0','MIT');
+INSERT INTO `about` VALUES (1,'Scott Grivner','scott.grivner@gmail.com','https://www.scottgrivner.dev','1.0.0','MIT');
 /*!40000 ALTER TABLE `about` ENABLE KEYS */;
 UNLOCK TABLES;
 
